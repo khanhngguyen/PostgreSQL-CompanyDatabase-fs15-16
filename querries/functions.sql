af5ab7ff6bdfb891a6cd35f209cecabd75fda6c4
khanhngguyen <ntkimkhanh97@gmail.com>
2023-07-07 13:57:51 +0300

    add function create_project_with_teams()

--- querries/functions.sql
+++ querries/functions.sql
@@ -1,3 +1,4 @@
+-- function track_working_hours
 CREATE OR REPLACE FUNCTION track_working_hours(employee_id integer, projectId integer, total_hours numeric(6, 2))
 RETURNS VOID
 AS 
@@ -36,3 +37,42 @@ LANGUAGE plpgsql;
 SELECT track_working_hours(20, 7, 40.5);
 SELECT track_working_hours(23, 7, 32.25);
 SELECT track_working_hours(12, 7, 22.75); --should raise exception
+
+-- function create_project_with_teams
+CREATE OR REPLACE FUNCTION create_project_with_teams(projectName VARCHAR(100), clientName VARCHAR(100), startDate date, endDate date, teams integer[])
+RETURNS VOID
+AS
+$$
+DECLARE
+	num integer;
+	projectId integer;
+BEGIN
+	IF EXISTS (SELECT * FROM public.projects WHERE project_name = projectName) THEN
+		RAISE EXCEPTION 'Project already exists';
+	ELSE
+		INSERT INTO public.projects(project_name, client, start_date, deadline)
+		VALUES(projectName, clientName, startDate, endDate);
+		SELECT id INTO projectId FROM public.projects WHERE project_name = projectName;
+		FOREACH num IN ARRAY teams
+			LOOP
+				IF NOT EXISTS (SELECT * FROM public.teams WHERE id = num) THEN
+						RAISE EXCEPTION 'Team ids do not exist';
+				ELSE
+					INSERT INTO public.team_project(team_id, project_id)
+					VALUES(num, projectId);
+				END IF;
+			END LOOP;
+	END IF;
+END;
+$$
+LANGUAGE plpgsql;
+
+-- testing function
+SELECT create_project_with_teams(
+    'New Project',
+    'Client XYZ',
+    '2023-07-01',
+    '2023-12-31',
+    ARRAY[1, 2, 3]
+);
+
